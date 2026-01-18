bc8e6b2d4b1deeca163746a9dda17a23aaa70ee2
softneer171 <softneer171@gmail.com>
2026-01-18 22:38:18 +0500

    objects

---
+++ object.js
@@ -0,0 +1,47 @@
+// 📝 Question 1: Create a JavaScript object named student, that stores the following information: Student name, Age, Course name, Is the student enrolled? (true / false). After creating the object, do the following tasks: Print the entire object in the console, Print only the student name using dot notation, Update the course name to "Web Development", Add a new property called feesPaid with value true, Delete the age property from the object, and Print the updated object in the console.
+
+let student = {
+    name: "John Doe",
+    age: 20,
+    course: "Computer Science",
+    isEnrolled: true
+};
+
+console.log("Original Student Object:", student);
+console.log("Student Name:", student.name);
+student.course = "Web Development";
+student.feesPaid = true;
+delete student.age;
+console.log("Updated Student Object:", student);
+
+
+// 📝 Question 2: Create a JavaScript object named course that stores the following details: Course title, Instructor name,Total duration (in months), Fees, An array of students enrolled. Now perform the following tasks: Print the course title and instructor name, Add a new student name to the students array, Update the fees value, Add a method called getCourseDetails() that prints all course information in a readable format, Use the method to display course details in the console, Check if the course duration is more than 3 months and print an appropriate message.
+
+let course = {
+    title: "JavaScript Basics",
+    instructor: "Sarah Smith",
+    durationMonths: 4,
+    fees: 500,
+    students: ["John", "Alice", "Mark"],
+
+    getCourseDetails: function () {
+        console.log("Course Title:", this.title);
+        console.log("Instructor:", this.instructor);
+        console.log("Duration (Months):", this.durationMonths);
+        console.log("Fees:", this.fees);
+        console.log("Students Enrolled:", this.students.join(", "));
+    }
+};
+
+console.log("Course Title:", course.title);
+console.log("Instructor Name:", course.instructor);
+course.students.push("Emma");
+course.fees = 600;
+console.log("\nCourse Details:");
+course.getCourseDetails();
+
+if (course.durationMonths > 3) {
+    console.log("This course duration is more than 3 months.");
+} else {
+    console.log("This course duration is 3 months or less.");
+}
