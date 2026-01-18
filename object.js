b003f3a394723f648e0c42cb357fbd0c679cd060
softneer171 <softneer171@gmail.com>
2026-01-18 22:40:13 +0500

    object

--- object.js
+++ object.js
@@ -1,9 +1,9 @@
 // 📝 Question 1: Create a JavaScript object named student, that stores the following information: Student name, Age, Course name, Is the student enrolled? (true / false). After creating the object, do the following tasks: Print the entire object in the console, Print only the student name using dot notation, Update the course name to "Web Development", Add a new property called feesPaid with value true, Delete the age property from the object, and Print the updated object in the console.
 
 let student = {
-    name: "John Doe",
+    name: "Amrha",
     age: 20,
-    course: "Computer Science",
+    course: "Software development",
     isEnrolled: true
 };
 
@@ -18,11 +18,11 @@ console.log("Updated Student Object:", student);
 // 📝 Question 2: Create a JavaScript object named course that stores the following details: Course title, Instructor name,Total duration (in months), Fees, An array of students enrolled. Now perform the following tasks: Print the course title and instructor name, Add a new student name to the students array, Update the fees value, Add a method called getCourseDetails() that prints all course information in a readable format, Use the method to display course details in the console, Check if the course duration is more than 3 months and print an appropriate message.
 
 let course = {
-    title: "JavaScript Basics",
-    instructor: "Sarah Smith",
+    title: "Web development",
+    instructor: "Hudair Younis",
     durationMonths: 4,
-    fees: 500,
-    students: ["John", "Alice", "Mark"],
+    fees: 15000,
+    students: ["Noor", "Fatima"] ,
 
     getCourseDetails: function () {
         console.log("Course Title:", this.title);
@@ -35,8 +35,8 @@ let course = {
 
 console.log("Course Title:", course.title);
 console.log("Instructor Name:", course.instructor);
-course.students.push("Emma");
-course.fees = 600;
+course.students.push("Zobia");
+course.fees = 16000;
 console.log("\nCourse Details:");
 course.getCourseDetails();
 
